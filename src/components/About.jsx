799a15150c14ef9235f4b49a4ca08cb48fc1144f
offyst <IKARTIKEYSHARMA@GMAIL.COM>
2024-10-31 11:13:19 +0530

    removed feedbacks, exp

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        A dynamic team of tech enthusiasts passionate about innovative problem-solving.
+        We specialize in development, design, and project execution, blending diverse skills 
+        to deliver impactful solutions. With a commitment to collaboration and learning, 
+        Hack-PAKT is dedicated to creating meaningful, cutting-edge tech that makes a difference.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
