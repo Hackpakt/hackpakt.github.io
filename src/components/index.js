2a77db369b00ec9dbf24f5320c9048eb60b83b2d
offyst <IKARTIKEYSHARMA@GMAIL.COM>
2024-11-01 14:56:23 +0530

    updated

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, StarsCanvas } from './canvas';
+import { StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -16,6 +16,5 @@ export {
   Works,
   Contact,
   CanvasLoader,
-  EarthCanvas, 
   StarsCanvas
 };
